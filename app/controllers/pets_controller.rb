dafdaae0a051cd689adbee3dd2379be8d58ca83f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-01-24 18:50:42 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -30,7 +30,7 @@ class PetsController < ApplicationController
     binding.pry
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-    @pet.owner = Owner.create(params[:owner]) unless params[:owner].empty?
+    @pet.owner = Owner.create(params[:owner]) unless params[:owner][:name].empty?
     @pet.save
     redirect to "pets/#{@pet.id}"
   end
